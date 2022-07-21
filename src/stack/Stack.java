b8dc269d80ec927d643928221a1b39a0d6485d42
pinheirocarlos <ti.carlospinheiro@gmail.com>
2022-07-20 21:32:43 -0300

    Implementando stack

---
+++ src/stack/Stack.java
@@ -0,0 +1,18 @@
+package stack;
+
+public class Stack {
+
+/*
+* Pilha é uma estrutura de dados que pode ser implementada com arrays ou linkedlist.
+* Possui abstração para 3 operações: push, pop e peek.
+* push é responsável por incluir elementos na pilha, pop faz a retirada do último elemento e peek retorna o
+* elemento mais recente sem removê-lo da pilha.
+* Em JAVA é implementado a memória stack, na qual é responsável por armazenar as chamadas aos métodos bem como
+* as variáveis primitivas declaradas e as referências aos objetos. Esta memória possui como característica uma baixa capacidade de
+* armazenamento mas um acesso muito rápido.
+* Além da memória stack, existe também a memória heap, que possui como característica uma capacidade
+* de armazenamento maior mas com um tempo de acesso inferior.
+* O espaço heap é responsável por armazenar os objetos instanciados.
+* */
+
+}
